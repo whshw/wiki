7eda8173ce7be4ac2bde568caba3100fbbaf2e8d
wanghs <wanghs0106@gmail.com>
2023-07-10 23:07:20 +0100

    2-8 增加自定义配置示例

--- src/main/java/com/whs/wiki/controller/TestController.java
+++ src/main/java/com/whs/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.whs.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${test.hello}")
+    private String testHello;
+
     @GetMapping("/hello")
     public String hello() {
 
-        return "Hello World!";
+        return "Hello World! " + testHello;
 
     }
 
