747dcc478d172223d39dc5f13bf7f182a7931910
wanghs <wanghs0106@gmail.com>
2023-07-10 22:55:15 +0100

    2-8 SpringBoot会自动识别下面这组配置文件:     application.properties/yml     config/application.properties/yml     如果是SpringCloud, 还会自动识别下面这组配置文件:     bootstrap.properties/yml     config/bootstrap.properties/yml

--- src/main/java/com/whs/wiki/controller/TestController.java
+++ src/main/java/com/whs/wiki/controller/TestController.java
@@ -2,10 +2,8 @@ package com.whs.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
 
 @RestController
 public class TestController {
